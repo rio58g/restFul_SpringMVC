1408b1aca8d216a3e86f5a727a94e95729153842
rio58g <rio58g@gmail.com>
2017-12-03 21:05:47 +0200

    add config + controller

---
+++ src/main/java/com/skorniychuk/config/WebConfig.java
@@ -0,0 +1,7 @@
+package com.skorniychuk.config;
+
+/**
+ * Created by ИРОЧКА on 03.12.2017.
+ */
+public class WebConfig {
+}
---
+++ src/main/java/com/skorniychuk/controller/ReminderController.java
@@ -0,0 +1,12 @@
+package com.skorniychuk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/remind")
+public class ReminderController {
+
+
+
+}
